ee988177d3935dbee128a8c9a6cf3ff3084b9d2f
Justin Joe <justin.aw.joe@gmail.com>
2021-12-04 17:59:05 +1300

    Clean up java day 3 part 2 solution

--- java/src/Day3/Part2.java
+++ java/src/Day3/Part2.java
@@ -19,107 +19,86 @@ public class Part2 {
     }
   }
 
-  public List<Tuple> findColumnDigits(List<String> numbers, int pos) {
-    List<Tuple> tuples = new ArrayList<>();
+  public List<Pair> findColumnDigits(List<String> numbers, int pos) {
+    List<Pair> pairs = new ArrayList<>();
     for (String num : numbers) {
-      tuples.add(new Tuple(Character.getNumericValue(num.charAt(pos)), num));
+      pairs.add(new Pair(Character.getNumericValue(num.charAt(pos)), num));
     }
-    return tuples;
+    return pairs;
   }
 
-  public int findOxyGenRating() {
+  public int findRating(boolean findOxy) {
     int pos = 0;
 
-    List<Tuple> tuples = findColumnDigits(inputs, pos);
-    while (tuples.size() > 1) {
-      List<Tuple> ones = new ArrayList<>();
-      List<Tuple> zeros = new ArrayList<>();
-      for (Tuple t : tuples) {
+    List<Pair> pairs = findColumnDigits(inputs, pos);
+
+    while (pairs.size() > 1) {
+      System.out.println(pairs);
+
+      List<Pair> ones = new ArrayList<>();
+      List<Pair> zeros = new ArrayList<>();
+      for (Pair t : pairs) {
         if (t.digit == 1) {
           ones.add(t);
         } else {
           zeros.add(t);
         }
       }
-      if (ones.size() > zeros.size()) {
-        tuples = ones;
-      } else if (ones.size() == zeros.size()) {
-        tuples = ones;
-      } else {
-        tuples = zeros;
-      }
-      if (tuples.size() == 1) {
-        return Integer.parseInt(tuples.get(0).binNum, 2);
-      }
-      pos++;
-      List<String> inputs = new ArrayList<>();
-      for (Tuple t : tuples) {
-        inputs.add(t.binNum);
-      }
-      tuples = findColumnDigits(inputs, pos);
-    }
-    System.out.println(tuples);
-    return Integer.parseInt(tuples.get(0).binNum, 2);
-  }
 
-  public int findCarbScrubRating() {
-    int pos = 0;
-
-    List<Tuple> tuples = findColumnDigits(inputs, pos);
-    while (tuples.size() > 1) {
-      System.out.println(tuples);
-      List<Tuple> ones = new ArrayList<>();
-      List<Tuple> zeros = new ArrayList<>();
-      for (Tuple t : tuples) {
-        if (t.digit == 1) {
-          ones.add(t);
+      if (findOxy) {
+        if (ones.size() >= zeros.size()) {
+          pairs = ones;
         } else {
-          zeros.add(t);
+          pairs = zeros;
+        }
+      } else {
+        if (zeros.size() <= ones.size()) {
+          pairs = zeros;
+        } else {
+          pairs = ones;
         }
       }
-      if (zeros.size() < ones.size()) {
-        tuples = zeros;
-      } else if (zeros.size() == ones.size()) {
-        tuples = zeros;
-      } else {
-        tuples = ones;
-      }
-      if (tuples.size() == 1) {
-        return Integer.parseInt(tuples.get(0).binNum, 2);
+
+      if (pairs.size() == 1) {
+        return Integer.parseInt(pairs.get(0).binNum, 2);
       }
       pos++;
+
       List<String> inputs = new ArrayList<>();
-      for (Tuple t : tuples) {
+      for (Pair t : pairs) {
         inputs.add(t.binNum);
       }
-      tuples = findColumnDigits(inputs, pos);
+      pairs = findColumnDigits(inputs, pos);
     }
-    System.out.println(tuples);
-    return Integer.parseInt(tuples.get(0).binNum, 2);
+
+    return Integer.parseInt(pairs.get(0).binNum, 2);
   }
+
 
   public static void main(String[] args) throws IOException {
     Part2 chall = new Part2();
     chall.sortData();
-    System.out.println("Oxygen generator rating: " + chall.findOxyGenRating());
-    System.out.println("\nCarbon scrubber rating: " + chall.findCarbScrubRating());
-    System.out.println("\nFinal multiplied result: " + chall.findOxyGenRating() * chall.findCarbScrubRating());
+    int oxyRating =  chall.findRating(true);
+    System.out.println("\nOxygen generator rating: " + oxyRating + "\n");
+    int carbRating =  chall.findRating(false);
+    System.out.println("\nCarbon scrubber rating: " + carbRating);
+    System.out.println("\nFinal multiplied result: " + oxyRating * carbRating);
   }
 }
 
-class Tuple {
+class Pair {
 
   int digit;
   String binNum;
 
-  public Tuple(int digit, String binNum) {
+  public Pair(int digit, String binNum) {
     this.digit = digit;
     this.binNum = binNum;
   }
 
   @Override
   public String toString() {
-    return "Tuple{" +
+    return "Pair{" +
             "digit=" + digit +
             ", binNum='" + binNum + '\'' +
             '}';
