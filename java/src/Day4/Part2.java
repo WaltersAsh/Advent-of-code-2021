8fc58549b611e97746048e933ab37a6a92082238
Justin Joe <justin.aw.joe@gmail.com>
2021-12-05 17:16:24 +1300

    Uploaded java solutions for day 4 (I'm an idiot)

--- java/src/Day4/Part2.java
+++ java/src/Day4/Part2.java
@@ -5,10 +5,14 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
-public class Part1 {
+public class Part2 {
 
   private List<Integer> calledNums;
   private List<Bingo> bingos;
@@ -105,91 +109,41 @@ public class Part1 {
     return unmarkedScore * lastNumCalled;
   }
 
-  public int process() {
+  public void process() {
+    Map<Integer, Integer> map = new LinkedHashMap<>();
+
     for (int num : calledNums) {
       for (Bingo bingo : bingos) {
+        if (bingo.hasWon) {
+          continue;
+        }
         mark(bingo, num);
       }
       for (Bingo bingo : bingos) {
+        if (bingo.hasWon) {
+          continue;
+        }
         if (hasWon(bingo)) {
-          return calculateScore(bingo, num);
+          int score = calculateScore(bingo, num);
+          map.put(score, num);
         }
       }
     }
-    return -1;
-  }
 
-  public void test() {
-
-    int[][] wub = {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9},
-    };
-
-    for (int col = 0; col < wub[0].length; col++) {
-      for (int row = 0; row < wub.length; row++) {
-        System.out.print(wub[row][col] + " ");
-      }
+    Set<Integer> keys = map.keySet();
+    Iterator<Integer> it = keys.iterator();
+    Integer last = 0;
+    while (it.hasNext()) {
+      last = it.next();
     }
-    System.out.println(" ");
-    for (int row = 0; row < wub.length; row++) {
-      for (int col = 0; col < wub[0].length; col++) {
-        System.out.print(wub[row][col] + " ");
-      }
-    }
+    System.out.println("Last board to win score: " + last);
   }
 
-  public void testerino() {
-    calledNums = Arrays.asList(7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19,
-            3, 26, 1);
-    bingos = new ArrayList<>();
-
-    int[][] one = new int[][]{
-            {22, 13, 17, 11, 0},
-            {8, 2, 23, 4, 24},
-            {21, 9, 14, 16, 7},
-            {6, 10, 3, 18, 5},
-            {1, 12, 20, 15, 19}
-    };
-
-    int[][] two = new int[][]{
-            {3, 15, 0, 2, 22},
-            {9, 18, 13, 17, 5},
-            {19, 8, 7, 25, 23},
-            {20, 11, 10, 24, 4},
-            {14, 21, 16, 12, 6}
-
-    };
-
-    int[][] three = new int[][]{
-            {14, 21, 17, 24, 4},
-            {10, 16, 15, 9, 19},
-            {18, 8, 23, 26, 20},
-            {22, 11, 13, 6, 5},
-            {2, 0, 12, 3, 7}
-
-    };
-    bingos.add(new Bingo(one, false));
-    bingos.add(new Bingo(two, false));
-    bingos.add(new Bingo(three, false));
-  }
 
   public static void main(String[] args) throws IOException {
-    Part1 chall = new Part1();
+    Part2 chall = new Part2();
     chall.readData();
-    //chall.testerino();
-    System.out.println("First board to win score: " + chall.process());
+    chall.process();
   }
 }
 
-class Bingo {
-  int[][] board;
-  int[][] originalBoard;
-  boolean hasWon;
-
-  public Bingo(int[][] board, boolean hasWon) {
-    this.board = board;
-    this.hasWon = hasWon;
-  }
-}
